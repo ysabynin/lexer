fe4c3e451fb79d01ee3de1c0015afb38e984ec63
ysabynin <evgensabyka@mail.ru>
2016-10-22 17:32:57 +0300

    added calls to new statements

--- test.bat
+++ test.bat
@@ -12,4 +12,10 @@ java -cp . Parser ./test/text6.txt
 java -cp . Parser ./test/text7.txt
 java -cp . Parser ./test/text8.txt
 java -cp . Parser ./test/text9.txt
+java -cp . Parser ./test/vtext10.txt
+java -cp . Parser ./test/vtext11.txt
+java -cp . Parser ./test/vtext12.txt
+java -cp . Parser ./test/vtext13.txt
+java -cp . Parser ./test/vtext14.txt
+java -cp . Parser ./test/vtext15.txt
 pause
